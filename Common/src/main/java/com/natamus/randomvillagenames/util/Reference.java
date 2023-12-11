2fffe2dcbe98aa0917296eca7c89c13f8fb4a68b
Rick South <rick@natamus.com>
2023-12-12 00:20:53 +0100

    Random Village Names source code for 1.20.4.

---
+++ Common/src/main/java/com/natamus/randomvillagenames/util/Reference.java
@@ -0,0 +1,8 @@
+package com.natamus.randomvillagenames.util;
+
+public class Reference {
+	public static final String MOD_ID = "randomvillagenames";
+	public static final String NAME = "Random Village Names";
+	public static final String VERSION = "3.4";
+	public static final String ACCEPTED_VERSIONS = "[1.20.4]";
+}
