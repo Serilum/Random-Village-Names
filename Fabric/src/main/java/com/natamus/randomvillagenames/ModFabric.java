8efbcfcde7d3b53cfaf4a2128d4ce6e6c3aa9170
Rick South <rick@natamus.com>
2024-06-14 05:15:16 +0200

    Random Village Names source code for 1.21.0.

---
+++ Fabric/src/main/java/com/natamus/randomvillagenames/ModFabric.java
@@ -0,0 +1,37 @@
+package com.natamus.randomvillagenames;
+
+import com.natamus.collective.check.RegisterMod;
+import com.natamus.randomvillagenames.events.SetVillageSignEvent;
+import com.natamus.randomvillagenames.util.Reference;
+import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.event.lifecycle.v1.ServerChunkEvents;
+import net.fabricmc.fabric.api.event.lifecycle.v1.ServerTickEvents;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.level.chunk.LevelChunk;
+
+public class ModFabric implements ModInitializer {
+	
+	@Override
+	public void onInitialize() {
+		setGlobalConstants();
+		ModCommon.init();
+
+		loadEvents();
+
+		RegisterMod.register(Reference.NAME, Reference.MOD_ID, Reference.VERSION, Reference.ACCEPTED_VERSIONS);
+	}
+
+	private void loadEvents() {
+		ServerTickEvents.START_WORLD_TICK.register((ServerLevel level) -> {
+			SetVillageSignEvent.onWorldTick(level);
+		});
+
+		ServerChunkEvents.CHUNK_LOAD.register((ServerLevel level, LevelChunk chunk) -> {
+			SetVillageSignEvent.onChunkLoad(level, chunk);
+		});
+	}
+
+	private static void setGlobalConstants() {
+
+	}
+}
