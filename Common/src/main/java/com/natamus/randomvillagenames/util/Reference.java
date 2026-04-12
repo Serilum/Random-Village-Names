272df99a5ccad9abe7de56042686aaa974e275f7
Rick South <rick@natamus.com>
2026-04-12 17:27:49 +0200

    (v4.2) (26.1.1/2) Fixed Areas version reference.

--- Common/src/main/java/com/natamus/randomvillagenames/util/Reference.java
+++ Common/src/main/java/com/natamus/randomvillagenames/util/Reference.java
@@ -3,6 +3,6 @@ package com.natamus.randomvillagenames.util;
 public class Reference {
 	public static final String MOD_ID = "randomvillagenames";
 	public static final String NAME = "Random Village Names";
-	public static final String VERSION = "4.1";
+	public static final String VERSION = "4.2";
 	public static final String ACCEPTED_VERSIONS = "[26.1.2]";
 }
